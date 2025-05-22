cc0d803dfca510a44a4dcec33c917318d17b5dee
mariana-molina <marsmolina20@gmail.com>
2025-05-22 20:56:25 +0200

    Change image config to remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['cevoid-dev-storage.b-cdn.net'],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cevoid-dev-storage.b-cdn.net',
+			},
+		],
 	},
 };
 
